5be20ad64f29921c8c9acc1945eb911ee0d43588
Trayan Azarov <tazarov@users.noreply.github.com>
2024-04-07 19:42:06 +0300

    feat: Chroma export usability (#140)
    Made chroma export command more usable by splitting the CLI parts into a separate function than the core export function that only interacts with Chroma. Writing to stdin or file is now done within the CLI part
    
    Refs: #129

--- chroma_dp/chroma/chroma_export.py
+++ chroma_dp/chroma/chroma_export.py
@@ -69,44 +69,19 @@ def read_large_data_in_chunks(
 
 
 def chroma_export(
-    uri: Annotated[str, typer.Argument(help="The Chroma endpoint.")],
-    collection: Annotated[
-        Optional[str], typer.Option(help="The Chroma collection.")
-    ] = None,
-    export_file: Optional[str] = typer.Option(
-        None, "--out", help="Export .jsonl file."
-    ),
-    append: Annotated[bool, typer.Option(help="Append to export file.")] = False,
-    limit: Annotated[int, typer.Option(help="The limit.")] = -1,
-    offset: Annotated[int, typer.Option(help="The offset.")] = 0,
-    batch_size: Annotated[int, typer.Option(help="The batch size.")] = 100,
-    embed_feature: Annotated[
-        str, typer.Option(help="The embedding feature.")
-    ] = "embedding",
-    meta_features: Optional[List[str]] = typer.Option(None,help="The metadata features."),
-    id_feature: Annotated[str, typer.Option(help="The id feature.")] = "id",
-    doc_feature: Annotated[
-        str, typer.Option(help="The document feature.")
-    ] = "text_chunk",
-    where: Optional[str] = typer.Option(
-        None,
-        "--where",
-        "-m",
-        help='Metadata filter. JSON with Chroma syntax is expected - \'{"metadata_key": "metadata_value"}\'',
-    ),
-    where_document: Optional[str] = typer.Option(
-        None,
-        "--where-document",
-        "-d",
-        help='Document filter string - JSON with Chroma syntax is expected - \'{"$contains": "search for this"}\'',
-    ),
-    format_output: Optional[str] = typer.Option(
-        "record", "--format",
-        help="Export format. Default is `record`. Supported formats: `record` and `jsonl`. "
-    ),
-) -> None:
-    if uri is None:
-        raise ValueError("Please provide a ChromaDP URI.")
+    uri: str,
+    collection: Optional[str] = None,
+    limit: Optional[int] = -1,
+    offset: Optional[int] = 0,
+    batch_size: Optional[int] = 100,
+    embed_feature: Optional[str] = "embedding",
+    meta_features: Optional[List[str]] = None,
+    id_feature: Optional[str] = "id",
+    doc_feature: Optional[str] = "text_chunk",
+    where: Optional[str] = None,
+    where_document: Optional[str] = None,
+    format_output: Optional[str] = "record",
+) -> List[Dict[str, Any]]:
     parsed_uri = CDPUri.from_uri(uri)
     client = get_client_for_uri(parsed_uri)
     _collection = parsed_uri.collection or collection
@@ -123,9 +98,6 @@ def chroma_export(
     _where_document = None
     if where_document:
         _where_document = validate_where_document(json.loads(where_document))
-    if export_file and not append:
-        with open(export_file, "w") as f:
-            f.write("")
     for offset in range(_start, total_results_to_fetch, _batch_size):
         _results = _get_result_to_chroma_doc_list(
             read_large_data_in_chunks(
@@ -151,10 +123,71 @@ def chroma_export(
             ]
         else:
             raise ValueError(f"Unsupported format: {format}")
-        if export_file:
-            with open(export_file, "a") as f:
-                for _doc in _final_results:
-                    f.write(str(json.dumps(_doc)) + "\n")
-        else:
+        return _final_results
+
+
+def chroma_export_cli(
+    uri: Annotated[str, typer.Argument(help="The Chroma endpoint.")],
+    collection: Annotated[
+        Optional[str], typer.Option(help="The Chroma collection.")
+    ] = None,
+    export_file: Optional[str] = typer.Option(
+        None, "--out", help="Export .jsonl file."
+    ),
+    append: Annotated[bool, typer.Option(help="Append to export file.")] = False,
+    limit: Annotated[int, typer.Option(help="The limit.")] = -1,
+    offset: Annotated[int, typer.Option(help="The offset.")] = 0,
+    batch_size: Annotated[int, typer.Option(help="The batch size.")] = 100,
+    embed_feature: Annotated[
+        str, typer.Option(help="The embedding feature.")
+    ] = "embedding",
+    meta_features: Optional[List[str]] = typer.Option(
+        None, help="The metadata features."
+    ),
+    id_feature: Annotated[str, typer.Option(help="The id feature.")] = "id",
+    doc_feature: Annotated[
+        str, typer.Option(help="The document feature.")
+    ] = "text_chunk",
+    where: Optional[str] = typer.Option(
+        None,
+        "--where",
+        "-m",
+        help='Metadata filter. JSON with Chroma syntax is expected - \'{"metadata_key": "metadata_value"}\'',
+    ),
+    where_document: Optional[str] = typer.Option(
+        None,
+        "--where-document",
+        "-d",
+        help='Document filter string - JSON with Chroma syntax is expected - \'{"$contains": "search for this"}\'',
+    ),
+    format_output: Optional[str] = typer.Option(
+        "record",
+        "--format",
+        help="Export format. Default is `record`. Supported formats: `record` and `jsonl`. ",
+    ),
+) -> None:
+    if export_file and not append:
+        with open(export_file, "w") as f:
+            f.write("")
+    _final_results = chroma_export(
+        uri=uri,
+        collection=collection,
+        limit=limit,
+        offset=offset,
+        batch_size=batch_size,
+        embed_feature=embed_feature,
+        meta_features=meta_features,
+        id_feature=id_feature,
+        doc_feature=doc_feature,
+        where=where,
+        where_document=where_document,
+        format_output=format_output,
+    )
+
+    if export_file:
+        with open(export_file, "a") as f:
             for _doc in _final_results:
-                typer.echo(json.dumps(_doc))
+                f.write(str(json.dumps(_doc)) + "\n")
+    else:
+        for _doc in _final_results:
+            typer.echo(json.dumps(_doc))
