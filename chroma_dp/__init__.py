11811467cf3ea4be1a71b3ce91d8e672fe8849f3
Krishn Parasar <76171905+Krishn1412@users.noreply.github.com>
2024-09-12 19:18:03 +0530

    Making chromadb package optional dependency

--- chroma_dp/__init__.py
+++ chroma_dp/__init__.py
@@ -1,3 +1,10 @@
+try:
+    import chromadb  # noqa: F401
+except ImportError:
+    raise ValueError(
+        "The chromadb is not installed. This package (chromadbx) requires that Chroma is installed to work. "
+        "Please install it with `pip install chromadb`"
+    )
 from typing import (
     Optional,
     Sequence,
